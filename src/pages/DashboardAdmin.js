a59a3a86b91f58f06b83d4cd98f23eb57e303ec6
Febu <21-08117@g.batstate-u.edu.ph>
2024-10-11 11:51:23 +0800

    added another bar chart for the total no. of stud-perf per campus
    more updates to come!

--- src/pages/DashboardAdmin.js
+++ src/pages/DashboardAdmin.js
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-//import Checkbox from '@mui/material/Checkbox';
-//import FormControlLabel from '@mui/material/FormControlLabel';
-//import { ChartContainer } from '@mui/x-charts/ChartContainer';
 import { ResponsiveChartContainer } from '@mui/x-charts/ResponsiveChartContainer';
 import { BarPlot } from '@mui/x-charts/BarChart';
 import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
-import { red } from '@mui/material/colors';
 
 export function DashboardAdmin() {
-    const [isResponsive, setIsResponsive] = React.useState(false);
-
   const Container = ResponsiveChartContainer;
   return (
     <Box sx={{ width: '90%' }}>
@@ -33,9 +27,32 @@ export function DashboardAdmin() {
           ]}
         >
           <BarPlot />
-          <ChartsXAxis label="Total Awards per Cultural Group" labelStyle={{color:red}} position="bottom" axisId="x-axis-id" />
+          <ChartsXAxis label="Total Awards per Cultural Group" position="bottom" axisId="x-axis-id" />
         </Container>
       </Paper>
+
+      <Paper sx={{ width: '100%', height: 300, mt: 2}} elevation={3} stack>
+        {/* @ts-ignore */}
+        <Container
+          series={[
+            {
+              type: 'bar',
+              data: [61, 72, 55, 41, 46, 53],
+            },
+          ]}
+          xAxis={[
+            {
+              data: ['Pablo Borbon', 'Alangilan', 'Lobo', 'Rosario', 'Lipa', 'Malvar'],
+              scaleType: 'band',
+              id: 'x-axis-id',
+            },
+          ]}
+        >
+          <BarPlot />
+          <ChartsXAxis label="Total Number of Student-Performers Per Campus" position="bottom" axisId="x-axis-id" />
+        </Container>
+      </Paper>
+
     </Box>
   );
 }
