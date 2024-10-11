dd9a95c4532452ac324b76dde3fcd00e6f90cfe4
Febu <21-08117@g.batstate-u.edu.ph>
2024-10-11 09:19:48 +0800

    Added a bar chart to the dashboard
    still customizing it

--- src/pages/DashboardAdmin.js
+++ src/pages/DashboardAdmin.js
@@ -1,9 +1,41 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+//import Checkbox from '@mui/material/Checkbox';
+//import FormControlLabel from '@mui/material/FormControlLabel';
+//import { ChartContainer } from '@mui/x-charts/ChartContainer';
+import { ResponsiveChartContainer } from '@mui/x-charts/ResponsiveChartContainer';
+import { BarPlot } from '@mui/x-charts/BarChart';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { red } from '@mui/material/colors';
+
 export function DashboardAdmin() {
-    return (
-        <>
-            <h1>
-                Dashboard
-            </h1>
-        </>
-    )
+    const [isResponsive, setIsResponsive] = React.useState(false);
+
+  const Container = ResponsiveChartContainer;
+  return (
+    <Box sx={{ width: '90%' }}>
+    <Paper sx={{ width: '100%', height: 300 }} elevation={3}>
+        {/* @ts-ignore */}
+        <Container
+          series={[
+            {
+              type: 'bar',
+              data: [1, 2, 3, 2, 1, 5],
+            },
+          ]}
+          xAxis={[
+            {
+              data: ['Dance', 'Literary', 'Music', 'Performing Arts', 'Theatre', 'Visual Arts'],
+              scaleType: 'band',
+              id: 'x-axis-id',
+            },
+          ]}
+        >
+          <BarPlot />
+          <ChartsXAxis label="Total Awards per Cultural Group" labelStyle={{color:red}} position="bottom" axisId="x-axis-id" />
+        </Container>
+      </Paper>
+    </Box>
+  );
 }
