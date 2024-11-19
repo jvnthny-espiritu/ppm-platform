106806f6d71c7fdd7cb460f52800fcaaf9ec387b
clair <clairarquiza@gmail.com>
2024-11-20 07:59:16 +0800

    moved the pdfs to public/assets para accessible, functional dl forms

--- front-end/src/pages/Forms.js
+++ front-end/src/pages/Forms.js
@@ -1,12 +1,67 @@
 export function Forms() {
   return (
     <>
-    <h1>Forms</h1>
+      <h1>Forms</h1>
       <div>
-      <a href = "./assets/pdfs/01_Certificate-of-Membership.pdf" target="_blank" class="link" attributes-list download>
-      Certificate of Membership
-      </a>
+        <ul>
+          <li>
+            <a
+              href="/assets/01_Certificate-of-Membership.pdf"
+              download="01_Certificate-of-Membership.pdf"
+            >
+              Certificate of Membership
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/02_Audition-Criteria.pdf"
+              download="02_Audition-Criteria.pdf"
+            >
+              Audition Criteria
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/03_Performers-Profile-Form.pdf"
+              download="03_Performers-Profile-Form.pdf"
+            >
+              Performers Profile Form
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/04_Request-for-Presentation-Intermission.pdf"
+              download="04_Request-for-Presentation-Intermission.pdf"
+            >
+              Request for Presentation Intermission
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/05_Trainer-Evaluation-Form.pdf"
+              download="05_Trainer-Evaluation-Form.pdf"
+            >
+              Trainer Evaluation Form
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/06_Property-Borrowing-Form.pdf"
+              download="06_Property-Borrowing-Form.pdf"
+            >
+              Property Borrowing Form
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/Audience-Evaluation-Form.pdf"
+              download="Audience-Evaluation-Form.pdf"
+            >
+              Audience Evaluation Form
+            </a>
+          </li>
+        </ul>
       </div>
     </>
-  )
+  );
 }
